c4f9d84898abb96f985477f0c4ae24c57434432c
diegotokenstate <diego@tokenstate.app>
2025-06-05 16:53:32 -0600

    Adjust HeroSection spacing

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -18,7 +18,7 @@ const HeroSection = () => {
       </div>
 
       {/* Content Container */}
-      <div className="relative z-10 container mx-auto px-4">
+      <div className="relative z-10 container mx-auto px-4 pt-20">
         <div className="max-w-4xl mx-auto text-center">
           {/* Logo Animation */}
           <motion.div
