d2340cfbbffc4b8cef8291d94855be7602f062a2
diegotokenstate <diego@tokenstate.app>
2025-06-05 18:55:42 -0600

    Update sponsors list in SponsorsSection

--- src/components/SponsorsSection.tsx
+++ src/components/SponsorsSection.tsx
@@ -12,129 +12,9 @@ interface Sponsor {
 
 const sponsors: Sponsor[] = [
   {
-    name: "Hornet Padel Pro",
-    image: "https://imgur.com/AudiMfN.jpg",
-    link: "https://www.instagram.com/hornetpadelpro/"
-  },
-  {
-    name: "Salsa del Impostor",
-    image: "https://imgur.com/4d1GxAS.jpg",
-    link: "https://www.instagram.com/salsadelimpostor/"
-  },
-  {
-    name: "Mare Co",
-    image: "https://imgur.com/Qg7NvBZ.jpg",
-    link: "https://www.instagram.com/mare__co/"
-  },
-  {
-    name: "Medusa Solar",
-    image: "https://imgur.com/PKqlmEQ.jpg",
-    link: "https://www.medusasolar.com/"
-  },
-  {
-    name: "Sangrita del Cuñado",
-    image: "https://imgur.com/vAl1KTL.jpg",
-    link: "https://www.instagram.com/sangrita.del.cunado/"
-  },
-  {
-    name: "Mural",
-    image: "https://imgur.com/oY5DYSn.jpg",
-    link: "https://www.mural.com.mx/"
-  },
-  {
-    name: "Smirnoff",
-    image: "https://imgur.com/v5rvPQP.jpg",
-    link: "https://www.instagram.com/smirnoffmx/"
-  },
-  {
-    name: "Come Verde",
-    image: "https://imgur.com/yLDFa4k.jpg",
-    link: "https://www.comeverde.mx/"
-  },
-  {
-    name: "Premier Nuts",
-    image: "https://imgur.com/ygE1EiV.jpg",
-    link: "https://www.instagram.com/premier.nuts/"
-  },
-  {
-    name: "Mint Dreams",
-    image: "https://imgur.com/paY1HOI.jpg",
-    link: "https://www.instagram.com/mintdreamsmx/"
-  },
-  {
-    name: "Six GDL",
-    image: "https://imgur.com/TJDIcf7.jpg",
-    link: "https://www.instagram.com/sixgdl/"
-  },
-  {
-    name: "Things Studio",
-    image: "https://imgur.com/zP7f9m4.jpg",
-    link: "https://www.instagram.com/thingsstudio_/"
-  },
-  {
-    name: "Das Avena",
-    image: "https://imgur.com/KTzgBs1.jpg",
-    link: "https://dasavena.com/"
-  },
-  {
-    name: "La Mar Restaurante",
-    image: "https://imgur.com/p4qhEhc.jpg",
-    link: "https://www.instagram.com/lamarrestaurante/"
-  },
-  {
     name: "Birkenstock",
     image: "https://imgur.com/vx6TqhD.jpg",
     link: "https://www.instagram.com/birkenstockmexico/"
-  },
-  {
-    name: "Station 24 Fitness",
-    image: "https://imgur.com/Y7Yoc1o.jpg",
-    link: "https://www.instagram.com/station24fitness/"
-  },
-  {
-    name: "Betterware",
-    image: "https://imgur.com/ZFHJVOJ.jpg",
-    link: "https://www.instagram.com/betterwaremexico/"
-  },
-  {
-    name: "Jägermeister",
-    image: "https://imgur.com/VjqZkUP.jpg",
-    link: "https://www.instagram.com/jagermeistermx/"
-  },
-  {
-    name: "Biokinesic",
-    image: "https://imgur.com/OSSuOdU.jpg",
-    link: "https://www.instagram.com/biokinesic/"
-  },
-  {
-    name: "Present Adaptógenos",
-    image: "https://imgur.com/nZzJyYy.jpg",
-    link: "https://www.instagram.com/present.adaptogenos/"
-  },
-  {
-    name: "Skarch",
-    image: "https://imgur.com/lvxHkHB.jpg",
-    link: "https://www.instagram.com/skarchmexico/"
-  },
-  {
-    name: "Red Cola",
-    image: "https://imgur.com/MPA3x5d.jpg",
-    link: "https://www.instagram.com/redcolamx/"
-  },
-  {
-    name: "Fornino",
-    image: "https://imgur.com/Nuaalnf.jpg",
-    link: "https://www.instagram.com/fornino_gdl/"
-  },
-  {
-    name: "Cabotina",
-    image: "https://imgur.com/bpIm7zq.jpg",
-    link: "https://www.instagram.com/cabotina_gdl/"
-  },
-  {
-    name: "Casa San Matías",
-    image: "https://imgur.com/nmJEwEv.jpg",
-    link: "https://www.instagram.com/casasanmatiasoficial/"
   },
   {
     name: "Electrolit",
@@ -142,14 +22,19 @@ const sponsors: Sponsor[] = [
     link: "https://www.instagram.com/electrolit/"
   },
   {
-    name: "Aleta Azul",
-    image: "https://imgur.com/xtmSY1t.jpg",
-    link: "https://www.instagram.com/aletazulcompany/"
+    name: "Atajo Outdoors",
+    image: "https://imgur.com/rCo7Vxe.jpg",
+    link: "https://www.instagram.com/atajooutdoors/"
   },
   {
-    name: "Palmar Spritz",
-    image: "https://imgur.com/SY2GplZ.jpg",
-    link: "https://www.instagram.com/palmar_spritz/"
+    name: "Red Cola",
+    image: "https://imgur.com/MPA3x5d.jpg",
+    link: "https://www.instagram.com/redcolamx/"
+  },
+  {
+    name: "Betterware",
+    image: "https://imgur.com/ZFHJVOJ.jpg",
+    link: "https://www.instagram.com/betterwaremexico/"
   },
   {
     name: "Wu Nutrition",
@@ -157,19 +42,24 @@ const sponsors: Sponsor[] = [
     link: "https://wunutrition.com/"
   },
   {
-    name: "Atajo Outdoors",
-    image: "https://imgur.com/rCo7Vxe.jpg",
-    link: "https://www.instagram.com/atajooutdoors/"
+    name: "Medusa Solar",
+    image: "https://imgur.com/PKqlmEQ.jpg",
+    link: "https://www.medusasolar.com/"
   },
   {
-    name: "The Fulen Club",
-    image: "https://imgur.com/8BGKSkR.jpg",
-    link: "https://www.instagram.com/thefulenclub/"
+    name: "Skarch",
+    image: "https://imgur.com/lvxHkHB.jpg",
+    link: "https://www.instagram.com/skarchmexico/"
   },
   {
-    name: "Deyun Pharma",
-    image: "https://imgur.com/6rm2xR7.jpg",
-    link: "https://www.instagram.com/deyunpharmamx/"
+    name: "Premier Nuts",
+    image: "https://imgur.com/ygE1EiV.jpg",
+    link: "https://www.instagram.com/premier.nuts/"
+  },
+  {
+    name: "Come Verde",
+    image: "https://imgur.com/yLDFa4k.jpg",
+    link: "https://www.comeverde.mx/"
   }
 ];
 
