2c0bef82340b1326b0b7857bb2d1de3dfab610b9
diegotokenstate <diego@tokenstate.app>
2025-08-20 16:30:45 -0500

    Updated SponsorsPage.tsx

--- src/pages/SponsorsPage.tsx
+++ src/pages/SponsorsPage.tsx
@@ -24,7 +24,7 @@ const sponsors: Sponsor[] = [
   },
   {
     name: "Medusa Solar",
-    image: "https://imgur.com/PKqlmEQ.jpg",
+    image: "https://imgur.com/lfCE3Um.jpg",
     link: "https://www.medusasolar.com/"
   },
   {
